7335aa5ab26aff7ec7e5455fed93d220bf6d678d
prem prasad khanal <khnl.prem@gmail.com>
2021-08-09 10:27:01 +0545

    lets talk modal

--- js/utility-map.js
+++ js/utility-map.js
@@ -10,6 +10,23 @@ Primary use: Digital platform
 
 (function ($) {
   $(document).ready(function () {
+    function openModal() {
+      $('.common-button').on('click', function (e) {
+        e.preventDefault();
+        var targetId = $(this).attr('popup-link');
+        $('#' + targetId).addClass('open');
+      });
+    }
+    openModal();
+
+    function closeModal() {
+      $('.popup-footer .common-button, .close-icon').on('click', function (e) {
+        e.preventDefault();
+        $(this).closest('.popup').removeClass('open');
+      });
+    }
+    closeModal();
+
     /*==================================
    Toggle Button
  ==================================*/
@@ -44,20 +61,17 @@ Primary use: Digital platform
     stickyMenu();
 
     function smoothScroll() {
-      $('.main-navigation li a, .scroll-link,.lets-talk').on(
-        'click',
-        function (e) {
-          e.preventDefault();
+      $('.main-navigation li a').on('click', function (e) {
+        e.preventDefault();
 
-          $('html, body').animate(
-            {
-              scrollTop: $($(this).attr('href')).offset().top,
-            },
-            900,
-            'linear'
-          );
-        }
-      );
+        $('html, body').animate(
+          {
+            scrollTop: $($(this).attr('href')).offset().top,
+          },
+          900,
+          'linear'
+        );
+      });
     }
     smoothScroll();
     AOS.init();
